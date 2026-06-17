e39c849d1c9a40430c18b04e27d69f481e14337f
nazrul23-human <nazrul23-human@gmail.com>
2026-06-17 17:04:15 +0600

    Connect MongoDB Atlas database

--- drivefleet-server/index.js
+++ drivefleet-server/index.js
@@ -1,19 +1,53 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
+// Load .env
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Check .env values
+console.log("USER:", process.env.DB_USER);
+console.log("PASS:", process.env.DB_PASS);
+
+// MongoDB URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eychk9p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create Mongo Client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// MongoDB Connection
+async function run() {
+  try {
+    await client.connect();
+
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+run();
+
+// Test Route
 app.get("/", (req, res) => {
   res.send("DriveFleet Server Running");
 });
 
+// Server Start
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
